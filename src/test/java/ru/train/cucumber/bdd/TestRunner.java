4716e95be0a3f3d164c7570600886c5493b950e0
ya_semin <esemin83@gmail.com>
2018-12-01 23:24:42 +0300

    cucumber #3 added gradle task "cucumber"

--- src/test/java/ru/train/cucumber/bdd/TestRunner.java
+++ src/test/java/ru/train/cucumber/bdd/TestRunner.java
@@ -1,12 +1,18 @@
 package ru.train.cucumber.bdd;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
 
 
 // TODO - usual run as - gradle clean test - from terminal
 // TODO - extended runner from terminal !!!
 
-@CucumberOptions(features = {"classpath:test"}, plugin = {"pretty", "html:report"})
-public class TestRunner extends AbstractTestNGCucumberTests {}
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"classpath:scenarios"}
+        ,plugin = {"pretty", "html:report"}
+        ,glue = "ru/train/cucumber/bdd"
+                    )
+public class TestRunner {}
 
